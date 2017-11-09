17932b809cc45071b1a7a71dae19906a0f422663
AskhatB <baltabaev.a2509@gmail,com>
2017-11-09 10:44:04 +0600

    modified validation and fixed faults

--- admin_panel/app.js
+++ admin_panel/app.js
@@ -14,9 +14,9 @@ var app = new Vue({
 		clickedUser: {},
 		selected: null,
 		options: [
-			{ text:'Участок 1', value: '0'},
-			{ text:'Участок 2', value: '1'},
-			{ text:'Участок 3', value: '2'}
+		{ text:'Участок 1', value: '0'},
+		{ text:'Участок 2', value: '1'},
+		{ text:'Участок 3', value: '2'}
 		]
 	},
 
@@ -38,95 +38,95 @@ var app = new Vue({
 
 		saveUser: function(){
 			console.log(this.selected);
-			var formData = app.toFormData(app.newUser);
+			// var formData = app.toFormData(app.newUser);
 			if(this.newUser.l_name == "" || this.newUser.f_name == "" || this.newUser.iin == "" || this.selected == null){
 				this.addValidateMessage = "Вы не заполнили необходимые поля!";
 				return false;
 			}else{
-			this.showingAddModal = false;
-			this.addValidateMessage = "";
-			var user = { 
-				l_name: formData.get('l_name'),
-				f_name: formData.get('f_name'),
-				pat_name: formData.get('pat_name'),
-				iin: formData.get('iin'),
-				position: formData.get('position'),
-				area_id: this.selected
-			};
-
-			axios.post("http://localhost:8000/api/user/create", user)
-			.then(function(response){
-				app.newUser = {l_name: "", f_name: "", pat_name: "", iin: "", area_id: "", position: ""};
-				if(response.data.error){
-					error = true;
-					app.errorMessage = response.data.message;
-				}else{
-					success = true;
-					app.successMessage = response.data.message;
-					app.getAllUsers();
-				}
-			});
+				this.showingAddModal = false;
+				this.addValidateMessage = "";
+				var user = { id: this.newUser.id,
+					l_name: this.newUser.l_name,
+					f_name: this.newUser.f_name,
+					pat_name: this.newUser.pat_name,
+					iin: this.newUser.iin,
+					position: this.newUser.position,
+					area_id: this.selected
+				};
+				axios.post("http://localhost:8000/api/user/create", user)
+				.then(function(response){
+					app.newUser = {l_name: "", f_name: "", pat_name: "", iin: "", area_id: "", position: ""};
+					if(response.data.error){
+						error = true;
+						app.errorMessage = response.data.message;
+					}else{
+						success = true;
+						app.successMessage = response.data.message;
+						app.getAllUsers();
+					}
+				});
 			}
 		},
 
 		updateUser: function(){
-			console.log(this.clickedUser.l_name);
-			var formData = app.toFormData(app.clickedUser);
+			console.log(this.clickedUser);
+
+			// var formData = app.toFormData(app.clickedUser);
 			if(this.clickedUser.l_name == "" || this.clickedUser.f_name == "" || this.clickedUser.iin == "" || this.selected == null){
 				this.addValidateMessage = "Вы не заполнили необходимые поля!";
 				return false;
 			}else{
 				this.showingEditModal = false;
 				this.addValidateMessage= "";
-			var user = { id: formData.get('id'),
-			l_name: formData.get('l_name'),
-			f_name: formData.get('f_name'),
-			pat_name: formData.get('pat_name'),
-			iin: formData.get('iin'),
-			position: formData.get('position'),
-			area_id: this.selected
-		};
+				var user = { id: this.clickedUser.id,
+					l_name: this.clickedUser.l_name,
+					f_name: this.clickedUser.f_name,
+					pat_name: this.clickedUser.pat_name,
+					iin: this.clickedUser.iin,
+					position: this.clickedUser.position,
+					area_id: this.selected
+				};
 
-		axios.put("http://localhost:8000/api/user/"+ user.id, user)
-		.then(function(response){
-			app.clickedUser = {};
-			if(response.data.error){
-				app.errorMessage = response.data.message;
-			}else{
-				app.successMessage = response.data.message;
-				app.getAllUsers();
+				axios.put("http://localhost:8000/api/user/"+ user.id, user)
+				.then(function(response){
+					app.clickedUser = {};
+					if(response.data.error){
+						app.errorMessage = response.data.message;
+					}else{
+						app.successMessage = response.data.message;
+						app.getAllUsers();
+					}
+				});
 			}
-		});
-	}
-	},
+		},
 
-	deleteUser: function(){
-		var formData = app.toFormData(app.clickedUser);
-		axios.delete("http://localhost:8000/api/user/" + formData.get('id'), formData)
-		.then(function(response){
-			app.clikedUser = {};
-			if(response.data.error){
-				app.errorMessage = response.data.message;
-			}else{
-				app.successMessage = response.data.message;
-				app.getAllUsers();
-			}
-		});
-	},
+		deleteUser: function(){
+			axios.delete("http://localhost:8000/api/user/" + this.clickedUser.id)
+			.then(function(response){
+				app.clikedUser = {};
+				if(response.data.error){
+					app.errorMessage = response.data.message;
+				}else{
+					app.successMessage = response.data.message;
+					app.getAllUsers();
+				}
+			});
+		},
 
 
-	selectUser: function(user){
-		app.selected = user.area_id;
-		app.clickedUser = user;
-	},
+		selectUser: function(user){
+			app.selected = user.area_id;
+			app.clickedUser = user;
+			console.log(app.clickedUser);
+		},
 
-	toFormData: function(obj){
-		var form_data = new FormData();
-		for(var key in obj){
-			form_data.append(key, obj[key]);
-		}
-		return form_data;
-	},
+	// toFormData: function(obj){
+	// 	var form_data = new FormData();
+	// 	for(var key in obj){
+	// 		form_data.append(key, obj[key]);
+	// 	}
+	// 	return form_data;
+	// },
 
 	clearMessage: function(){
 		app.errorMessage = "";
